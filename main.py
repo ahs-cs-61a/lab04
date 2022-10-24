cda42e0bc4b85040682d77bd54c0b8e4fc8e51af
Robert Tian <74839761+Robertian23@users.noreply.github.com>
2022-10-24 10:48:06 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from labs.lab03 import *
+from labs.lab03_wwpd import *
+
+# WWPD? questions:  
+# un-comment one-by-one and complete question set
+
+# wwpd_virfib_sq()
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='lab',
+  packages=find_packages()
+)
