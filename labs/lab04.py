ae4d300e9e7335a1ad3878b695853d3fc4575518
Robert Tian <roberttian0523@gmail.com>
2022-10-24 11:03:20 -0700

    make lab04

---
+++ labs/lab04.py
@@ -0,0 +1,180 @@
+# lab04 - containers, sequences, mutability
+
+
+# disc04: https://inst.eecs.berkeley.edu/~cs61a/su22/disc/disc04/
+
+
+def my_map(fn, seq): # q1
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def my_filter(pred, seq): # q2
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def my_reduce(combiner, seq): # q3
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def my_reduce(combiner, seq): # q4, do NOT use recursion or loops
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def even_weighted(s): # q5
+    """
+    >>> x = [1, 2, 3, 4, 5, 6]
+    >>> even_weighted(x)
+    [0, 6, 20]
+    """
+    return [] # one liner list comprehension, add code in brackets
+
+
+def max_product(s): # q6
+    """Return the maximum product that can be formed using
+    non-consecutive elements of s.
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+
+
+# lab04: https://inst.eecs.berkeley.edu/~cs61a/su22/lab/lab04/
+
+
+def flatten(s): # q7
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]     # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def insert_items(lst, entry, elem): # q8, do NOT create a new list
+    """Inserts elem into lst after each occurrence of entry and then returns lst.
+
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> double_lst = [1, 2, 1, 2, 3, 3]
+    >>> double_lst = insert_items(double_lst, 3, 4)
+    >>> double_lst
+    [1, 2, 1, 2, 3, 4, 3, 4]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def couple(s, t): # q9
+    """Return a list of two-element lists in which the i-th element is [s[i], t[i]].
+
+    >>> a = [1, 2, 3]
+    >>> b = [4, 5, 6]
+    >>> couple(a, b)
+    [[1, 4], [2, 5], [3, 6]]
+    >>> c = ['c', 6]
+    >>> d = ['s', '1']
+    >>> couple(c, d)
+    [['c', 's'], [6, '1']]
+    """
+    assert len(s) == len(t)
+    "*** YOUR CODE HERE ***"
+
+
+# hw04: https://inst.eecs.berkeley.edu/~cs61a/su22/hw/hw04/
+
+
+def merge(lst1, lst2): # q10
+    """Merges two sorted lists.
+
+    >>> s1 = [1, 3, 5]
+    >>> s2 = [2, 4, 6]
+    >>> merge(s1, s2)
+    [1, 2, 3, 4, 5, 6]
+    >>> s1
+    [1, 3, 5]
+    >>> s2
+    [2, 4, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    >>> merge([2, 3, 4], [2, 4, 6])
+    [2, 2, 3, 4, 4, 6]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def remove_odd_indices(lst, odd): # q11, do NOT use iteration
+    """Remove elements of lst that have odd indices. Use recursion!
+
+    >>> s = [1, 2, 3, 4]
+    >>> t = remove_odd_indices(s, True)
+    >>> s
+    [1, 2, 3, 4]
+    >>> t
+    [1, 3]
+    >>> l = [5, 6, 7, 8]
+    >>> m = remove_odd_indices(l, False)
+    >>> m
+    [6, 8]
+    >>> remove_odd_indices([9, 8, 7, 6, 5, 4, 3], False)
+    [8, 6, 4]
+    >>> remove_odd_indices([2], False)
+    []
+    """
+    "*** YOUR CODE HERE ***"
